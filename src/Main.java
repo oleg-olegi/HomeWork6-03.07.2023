09282f8ffd67f0962ae5964f377d8c28107f1548
User <ol.shink87@gmail.com>
2023-07-03 19:28:04 +0300

    задачи 5-7

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
+        task6();
+        task7();
+        task8();
     }
 
     public static void task1() {
@@ -35,5 +39,31 @@ public class Main {
         for (int i = 10; i >= -10; i--) {
             System.out.print(i + "\s");
         }
+    }
+
+    public static void task5() {
+        System.out.println("\nЗадача 5");
+        for (int i = 1904; i <= 2096; i += 4) {
+            System.out.println(i + " год является високосным");
+        }
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.print(i + "\s");
+            if (i == 98) {
+                System.out.print("\n");
+            }
+        }
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i *= 2) {
+            System.out.print(i + "\s");
+        }
+    }public static void task8(){
+        System.out.println("\nЗадача 8");
     }
 }
